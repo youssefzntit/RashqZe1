88a5295e9cacb3d5a738359d5e9539788a2d52c2
Mody <143101587+Source-Ze@users.noreply.github.com>
2023-11-07 10:23:34 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import pyrogram , pyromod
+
+from pyromod import listen
+
+from pyrogram import Client, filters, enums
+from kvsqlite.sync import Client as dt
+p = dict(root='plugins')
+tok = Config.TG_BOT_TOKEN ## توكنك 
+id = 6509622797 ## ايديك
+db = dt("data.sqlite", 'fuck')
+if not db.get("checker"):
+  db.set('checker', None)
+if not db.get("admin_list"):
+  db.set('admin_list', [id, 6509622797])
+if not db.get('ban_list'):
+  db.set('ban_list', [])
+if not db.get('sessions'):
+  db.set('sessions', [])
+if not db.get('force'):
+  db.set('force', ['ui_xb'])
+x = Client(name='loclhosst', api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
+
+x.run()
