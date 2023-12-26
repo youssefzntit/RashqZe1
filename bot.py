dc22556de7c8ff6128c795440e4387348b5ba132
youssefzntit <154595412+youssefzntit@users.noreply.github.com>
2023-12-26 14:55:25 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,13 +11,13 @@ db = dt("data.sqlite", 'fuck')
 if not db.get("checker"):
   db.set('checker', None)
 if not db.get("admin_list"):
-  db.set('admin_list', [id, 6509622797])
+  db.set('admin_list', [id, 6653027663])
 if not db.get('ban_list'):
   db.set('ban_list', [])
 if not db.get('sessions'):
   db.set('sessions', [])
 if not db.get('force'):
-  db.set('force', ['ui_xb'])
+  db.set('force', ['matrixspam'])
 x = Client(name='loclhosst', api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
 
 x.run()
