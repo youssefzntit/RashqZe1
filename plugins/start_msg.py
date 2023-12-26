d8305dc5723136e40bdc03ddc50aefdf9fdda0c1
youssefzntit <154595412+youssefzntit@users.noreply.github.com>
2023-12-26 14:04:14 +0000

    Update start_msg.py

--- plugins/start_msg.py
+++ plugins/start_msg.py
@@ -36,7 +36,7 @@ async def startm(app, msg):
             [btn(text='⦅ قسم الخدمات ⦆', callback_data='service')],
             [btn(text='⦅ تجميع الرصيد ⦆', callback_data='invite'), btn(text='⦅ شراء الرصيد ⦆', callback_data='buy')],
             [btn(text='⦅ معلومات حسابك ⦆', callback_data='account'), btn(text='⦅ تحويل الرصيد ⦆', callback_data='trans')],
-            [btn(text='قناة التحديثات', url='trprogram.t.me')]
+            [btn(text='قناة التحديثات', url='khidmatokom.t.me')]
         ]
     )
         rk = f'''
@@ -62,7 +62,7 @@ async def startm(app, msg):
             [btn(text='قسم خدمات الرشق', callback_data='service')],
             [btn(text='تجميع رصيد', callback_data='invite'), btn(text='شراء رصيد', callback_data='buy')],
             [btn(text='معلومات حسابك', callback_data='account'), btn(text='تحويل رصيد', callback_data='trans')],
-            [btn(text='قناة التحديثات', url='trprogram.t.me')]
+            [btn(text='قناة التحديثات', url='khidmatokom.t.me')]
         ]
     )
         rk =f'''
